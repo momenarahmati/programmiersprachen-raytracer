93f7b100ad7c45ddec93dce2225b77e8233a3005
Momena Rahmati <sosan.momeni@gmial.com>
2020-08-29 10:51:20 +0200

    add ray.hpp

---
+++ framework/ray.hpp
@@ -0,0 +1,25 @@
+#ifndef RAY_HPP
+#define RAY_HPP
+#include <glm/vec3.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtx/intersect.hpp>
+#include <iostream>
+
+//Datentransferobjekt Ray 
+struct Ray
+{
+    glm::vec3 origin = { 0.0f, 0.0f, 0.0f };
+    glm::vec3 direction = { 0.0f, 0.0f, -1.0f };
+
+    Ray() :
+        origin{ 0.0f, 0.0f, 0.0f },
+        direction{ 0.0f, 0.0f, -1.0f }
+    {}
+
+    Ray(glm::vec3 origin_, glm::vec3 direction_) :
+        origin{ origin_ },
+        direction{ glm::normalize(direction_) } {}
+
+};
+
+#endif //RAY_HPP
